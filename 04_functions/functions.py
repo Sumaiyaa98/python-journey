2c8210204a7d743eb61ca6ab1a45aed4e0a88133
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-10 22:10:18 +0500

    add new files

---
+++ 04_functions/functions.py
@@ -0,0 +1,57 @@
+# 1. Basic Function Syntax
+# Problem: Write a function to calculate and return the square of a number.
+
+def square(num):
+   return num ** 2
+
+print(square(4))
+
+# 2. Function with Multiple Parameters
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+
+def sum(a,b):
+   return int(a)+int(b)
+
+print(sum('2',3))
+
+
+# 3. Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def mul(x,y):
+   return x * y
+
+print(mul(4,3))
+
+
+# 4. Function Returning Multiple Values
+# Problem: Create a function that returns both the area and circumference of a circle given its radius.
+
+import math
+def circle(radius):
+   circum = 2*math.pi*radius
+   area = math.pi*(radius**2)
+   return circum, area
+   
+
+a,b = circle(4)
+print("Circumference:",a.__round__(2),"Area:",b.__round__(2))
+
+# 5. Default Parameter Value
+# Problem: Write a function that greets a user. If no name is provided, it should greet with a default name.
+def greeting(user='User'):
+   return 'Hello '+ user + '!'
+
+print( greeting('Mike'))
+
+# 6. Lambda Function
+# Problem: Create a lambda function to compute the cube of a number.
+
+
+cube = lambda x: x ** 3
+
+print(cube(2))
+
+# 7. Function with *args
+# Problem: Write a function that takes variable number of arguments and returns their sum.
+
