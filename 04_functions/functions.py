0662fa87cd4e0af2df5555679089f3bffa01b89a
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-10 22:19:28 +0500

    add updated filess

--- 04_functions/functions.py
+++ 04_functions/functions.py
@@ -62,3 +62,6 @@ def sum_fun(*args):
     return sum(args)
 
 print(sum_fun(1, 2))
+
+# 8. Function with **kwargs
+# Problem: Create a function that accepts any number of keyword arguments and prints them in the format key: value.
