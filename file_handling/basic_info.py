92232431f58117c3f2a8b1fa3b2e17bbf012aaf5
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-12 21:23:33 +0500

    add updated code

--- file_handling/basic_info.py
+++ file_handling/basic_info.py
@@ -13,3 +13,17 @@ finally:
 #another way to make file in a better or short way
 with open('secondFile.txt','w') as file:
     file.write('my Second file')
+
+
+# we also have open('file','r') option as well which is used to read the file
+#  Why do we use with open('youtube.txt', 'r') as file:?
+# 'r' means read mode.
+
+# You use it when you just want to read the contents of a file.
+
+# This will not create the file if it doesn’t exist — instead, it will give an error.
+
+# 📌 What’s the difference between 'r' and 'w'?
+# Mode	What It Does
+# 'r'	Read file — file must exist or you'll get an error (FileNotFoundError).
+# 'w'	Write to file — if the file doesn’t exist, Python will create it. If it does exist, Python will overwrite it.
