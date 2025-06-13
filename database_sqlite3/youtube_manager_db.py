30c3de889c6a5cf7ac83038e91aab6f294f03997
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-13 19:03:27 +0500

    add file

---
+++ database_sqlite3/youtube_manager_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('youtube_video.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS videos (
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL
+               )
+
+''')
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
