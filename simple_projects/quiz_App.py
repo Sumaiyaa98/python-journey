2c5461acbfa374161785e80a0f4841486fc52e04
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-14 20:22:21 +0500

    add updated code

--- simple_projects/quiz_App.py
+++ simple_projects/quiz_App.py
@@ -1,6 +1,27 @@
-while(True):
-    print("Welcome to Quiz App!|Choose Option")
-    print('1-Start Quiz')
-    print('2-Show Quiz Result')
-    print('3-Exit')
-    
+def start_quiz():
+    pass
+def show_quiz_score():
+    pass
+
+
+
+def main():
+
+    while(True):
+        print("Welcome to Quiz App!|Choose Option")
+        print('1-Start Quiz')
+        print('2-Show Quiz Result')
+        print('3-Exit')
+        choice = input("Enter your Choice: ")
+        match(choice):
+            case '1':
+                start_quiz()
+            case '2':
+                show_quiz_score()
+            case '3':
+                break
+            case _:
+                print("Invalid Choice")
+
+if __name__ == '__main__':
+    main()
