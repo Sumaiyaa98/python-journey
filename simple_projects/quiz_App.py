8d78a4920ae8a74697f3d70d7665d7c7bc451f92
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-14 21:07:11 +0500

    add updated code

--- simple_projects/quiz_App.py
+++ simple_projects/quiz_App.py
@@ -1,24 +1,57 @@
 def start_quiz():
-    pass
-def show_quiz_score():
-    pass
+    print("\n*********Answer the Following Questions!*********\n")
+
+    questions = [
+        'What is the capital of France?',
+        'What is 5 + 7?',
+        'What is the highest mountain in the world?',
+        'What does CPU stand for?',
+        'What is the longest river in the world?'
+    ]
+
+    correct_ans = ['paris', '12', 'mount everest', 'central processing unit', 'nile']
+    user_ans = []
+
+    for i in questions:
+        q = input(f"{i} ")
+       
+        user_ans.append(q.lower())
+            
+
+    quiz = {}
+    for j in range(len(questions)):
+        quiz[questions[j]] = user_ans[j]
+
+    print('*' * 60)
+    for idx, (question, answer) in enumerate(quiz.items()):
+        print(f"\nQ: {question}\nYour Ans: {answer}\nCorrect Ans -> {correct_ans[idx]}")
+    print('*' * 60)
+
+    
+    count = 0
+    for i in range(len(user_ans)):
+        if correct_ans[i] == user_ans[i]:
+            count += 1
+
+    result = (count / len(correct_ans)) * 100
+    print(f"\nYour Score: {result}% ({count} out of {len(correct_ans)} correct)")
+
 
 
 
 def main():
 
     while(True):
-        print("Welcome to Quiz App!|Choose Option")
+        print('*'*60)
+        print("\nWelcome to Quiz App!|Choose Option")
         print('1-Start Quiz')
-        print('2-Show Quiz Result')
-        print('3-Exit')
+        print('2-Exit\n')
+        print('*'*60)
         choice = input("Enter your Choice: ")
         match(choice):
             case '1':
                 start_quiz()
             case '2':
-                show_quiz_score()
-            case '3':
                 break
             case _:
                 print("Invalid Choice")
