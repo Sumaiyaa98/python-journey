7b45e1ef5c9e91a702730fa2e5e953312de9c66e
Sumaiyaa98 <sumaiyaahmed1199@gmail.com>
2025-06-20 15:16:06 +0500

    add new file

---
+++ api_projects/country_info_finder.py
@@ -0,0 +1,59 @@
+import requests
+import json
+
+def load_data():
+    try:
+        with open('countryInfoFinderApp.txt','r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def save_data(info):
+    with open('countryInfoFinderApp.txt','w') as file:
+        json.dump(info,file,indent=4)
+
+
+def country_info_finder(country_name):
+    url = f"https://restcountries.com/v3.1/name/{country_name}"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        official_name = data[0]['name']['official']
+        region = data[0]['region']
+        population = data[0]['population']
+        currency = data[0]['currencies']
+        languages = data[0]['languages']
+        return official_name,region,population,currency,languages
+    else:
+        raise Exception("Country Details not found! Check spelling again")
+
+def main():
+
+    all_data = load_data()
+    print("Welcome to Country Info Finder App")
+    while True:
+
+        country_name = input("Enter a country name | (or type 'exit') : ").lower()
+        if country_name == 'exit':
+            break
+
+        try:
+            official_name,region,population,currency,languages = country_info_finder(country_name)
+            print(f"Official Name: {official_name}\nRegion: {region}\nPopulation: {population}\nCurrency: {currency}\nLanguages: {languages}")
+
+            countr_info = {
+                'Official Name': official_name,
+                'Region': region,
+                'Population': population,
+                'Currency': currency,
+                'Languages': languages
+
+            }
+            all_data.append(countr_info)
+            save_data(all_data)
+        except Exception as e:
+            print(str(e))
+
+
+if __name__ == '__main__':
+    main()
